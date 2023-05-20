7c07776567b06041bc07748e6c626cdeaad97519
Andrew Tolkachew <andrey.tolkachev899@gmail.com>
2023-05-20 22:42:02 +0300

    6/10 functions done

--- tempvoice.py
+++ tempvoice.py
@@ -1,6 +1,6 @@
 import discord
 import discord.utils
-from discord.ui import Button, View, Modal, InputText
+from discord.ui import Button, View, Modal, InputText, Select
 from discord.ext import commands
 import config
 from logs import logs
@@ -9,11 +9,12 @@ class tempvoice(commands.Cog):
     def __init__(self, client):
         self.client: commands.Bot = client
         self.temp_channels = []
+        self.current_response: discord.Interaction = None
 
     @commands.Cog.listener()
     async def on_ready(self):
         interface_embed = discord.Embed(title="tempvoice interface", description="", color=2829617)
-        interface_embed.set_image(url="https://media.discordapp.net/attachments/1101840195466301460/1105105018174062672/interface.png")
+        interface_embed.set_image(url="https://cdn.discordapp.com/attachments/1101840195466301460/1107693369158815744/interface.png")
 
         emoji_rename = discord.PartialEmoji(name="rename", animated=False, id=1101840047549984779)
         emoji_limit = discord.PartialEmoji(name="limit", animated=False, id=1101840054764183592)
@@ -28,16 +29,16 @@ class tempvoice(commands.Cog):
 
         interface_view = View(timeout=None)
 
-        button_rename = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_rename)
-        button_limit = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_limit)
-        button_private = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_private)
-        button_visibility = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_visibility)
-        button_region = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_region)
-        button_allow = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_allow)
-        button_forbid = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_forbid)
-        button_transfer = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_transfer)
-        button_kick = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_kick)
-        button_delete = Button(label="", style=discord.ButtonStyle.blurple, emoji=emoji_delete)
+        button_rename = Button(label="", custom_id="tempvoice_rename", style=discord.ButtonStyle.blurple, emoji=emoji_rename)
+        button_limit = Button(label="", custom_id="tempvoice_limit", style=discord.ButtonStyle.blurple, emoji=emoji_limit)
+        button_private = Button(label="", custom_id="tempvoice_private", style=discord.ButtonStyle.blurple, emoji=emoji_private)
+        button_visibility = Button(label="", custom_id="tempvoice_visibility", style=discord.ButtonStyle.blurple, emoji=emoji_visibility)
+        button_region = Button(label="", custom_id="tempvoice_region", style=discord.ButtonStyle.blurple, emoji=emoji_region)
+        button_allow = Button(label="", custom_id="tempvoice_allow", style=discord.ButtonStyle.blurple, emoji=emoji_allow)
+        button_forbid = Button(label="", custom_id="tempvoice_forbid", style=discord.ButtonStyle.blurple, emoji=emoji_forbid)
+        button_transfer = Button(label="", custom_id="tempvoice_transfer", style=discord.ButtonStyle.blurple, emoji=emoji_transfer)
+        button_kick = Button(label="", custom_id="tempvoice_kick", style=discord.ButtonStyle.blurple, emoji=emoji_kick)
+        button_delete = Button(label="", custom_id="tempvoice_delete", style=discord.ButtonStyle.blurple, emoji=emoji_delete)
 
         button_rename.callback = self.rename
         button_limit.callback = self.limit
@@ -61,78 +62,183 @@ class tempvoice(commands.Cog):
         interface_view.add_item(button_kick)
         interface_view.add_item(button_delete)
 
+        # раскомментить если нету сообщения
         await self.client.get_channel(config.interface_channel).purge(limit=100)
         await self.client.get_channel(config.interface_channel).send(view=interface_view, embed=interface_embed)
 
+        #self.client.add_view(interface_view)
+
     @commands.Cog.listener()
     async def on_voice_state_update(self, member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
-        log_channel = self.client.get_channel(config.voice_log)
         # Tempvoice creation
         if after.channel != None and after.channel.id == config.tempvoice_channel:
             await self.voice_create(member)
         # Tempvoice deletion
-        for t in self.temp_channels:
-            if before.channel != None and before.channel == t.get("channel") and before.channel.members == []:
-                await before.channel.delete()
-                embed:discord.Embed = await logs.log_embed(
-                    self=self,
-                    title=f"{self.client.user.name}",
-                    description=f"<:red:1097244281816748072> tempvoice `#{before.channel.name}` was deleted",
-                    user=self.client.user
-                    )
-                await log_channel.send(embed=embed)
-                self.temp_channels.remove(t)
+        for channel_owner in self.temp_channels:
+            if before.channel != None and before.channel == channel_owner.get("channel") and before.channel.members == []:
+                self.temp_channels.remove(channel_owner)
+                await self.voice_delete(before.channel)
 
+    async def voice_delete(self, channel: discord.VoiceChannel):
+        log_channel = self.client.get_channel(config.voice_log)
+        await channel.delete()
+        embed:discord.Embed = await logs.log_embed(
+            self=self,
+            title=f"{self.client.user.name}",
+            description=f"<:red:1097244281816748072> tempvoice `#{channel.name}` was deleted",
+            user=self.client.user
+            )
+        await log_channel.send(embed=embed)
+    
     async def voice_create(self, member: discord.Member):
         log_channel = self.client.get_channel(config.voice_log)
+        role: discord.Role = discord.utils.get(member.guild.roles, id=config.default_role)
+        unverified: discord.Role = discord.utils.get(member.guild.roles, id=config.unverified_role)
         create_channel = self.client.get_channel(config.tempvoice_channel)
         temp_channel = await create_channel.category.create_voice_channel(member.name)
-        self.temp_channels.append({'channel': temp_channel, 'owner_id': member.id})
+        self.temp_channels.append({'channel': temp_channel, 'owner_id': member.id, 'members': []})
         embed:discord.Embed = await logs.log_embed(
             self=self,
             title=f"{self.client.user.name}",
             description=f"<:green:1097244269267402812> tempvoice <#{temp_channel.id}> was created",
             user=self.client.user
             )
+        await temp_channel.set_permissions(member, connect=True)
+        await temp_channel.set_permissions(role, connect=True, view_channel=True)
+        await temp_channel.set_permissions(unverified, view_channel=False)
         await log_channel.send(embed=embed)
         await member.move_to(temp_channel)
 
-    async def rename(self, ctx: discord.Interaction):
+    def permission_check(func):
+        async def wrapper(self, ctx: discord.Interaction):
+            for channel_owner in self.temp_channels:
+                if channel_owner.get("owner_id") == ctx.user.id:
+                    await func(self, ctx, channel_owner)
+                    return wrapper
+            if self.current_response and ctx.user.id == self.current_response.user.id: await self.current_response.delete_original_response()
+            self.current_response = await ctx.response.send_message("you do not have permission", ephemeral=True)
+        return wrapper
+
+    @permission_check
+    async def rename(self, ctx: discord.Interaction, channel_owner):
         rename_modal = Modal(title="Rename")
-        rename_modal.add_item(InputText(label="Enter new channel name"))
+        rename_modal.add_item(InputText(label="Enter new channel name", max_length=100))
         async def callback(interaction: discord.Interaction):
-            await self.client.get_channel(t.get("channel").id).edit(name=rename_modal.children[0].value)
-            await interaction.response.send_message(content="name changed", ephemeral=True)
+            channel: discord.VoiceChannel = channel_owner.get("channel")
+            await channel.edit(name=rename_modal.children[0].value)
+            if self.current_response and ctx.user.id == self.current_response.user.id: await self.current_response.delete_original_response()
+            self.current_response = await interaction.response.send_message(content="name changed", ephemeral=True)
         rename_modal.callback = callback
-        for t in self.temp_channels:
-            if t.get("owner_id") == ctx.user.id:
-                await ctx.response.send_modal(rename_modal)
-                return
-        await ctx.response.send_message(content="пошел нахуй", ephemeral=True)
+        await ctx.response.send_modal(rename_modal)
 
-    async def limit():
+    @permission_check
+    async def limit(self, ctx: discord.Interaction, channel_owner):
+        limit_modal = Modal(title="Limit")
+        limit_modal.add_item(InputText(
+            label="Enter new limit for your channel", 
+            placeholder="Leave blank to reset limit",
+            required=False,
+            max_length=2
+            ))
+        async def callback(interaction: discord.Interaction):
+            value = limit_modal.children[0].value
+            channel: discord.VoiceChannel = channel_owner.get("channel")
+            if value != "" and value.isnumeric():
+                await channel.edit(user_limit=limit_modal.children[0].value)
+            else:
+                await channel.edit(user_limit=0)
+            if self.current_response and ctx.user.id == self.current_response.user.id: await self.current_response.delete_original_response()
+            self.current_response = await interaction.response.send_message(content="limit changed", ephemeral=True)
+        limit_modal.callback = callback
+        await ctx.response.send_modal(limit_modal)
+
+    @permission_check
+    async def private(self, ctx: discord.Interaction, channel_owner):
+        channel: discord.VoiceChannel = channel_owner.get("channel")
+        role: discord.Role = discord.utils.get(ctx.guild.roles, id=config.default_role)
+        permissions: discord.Permissions = channel.permissions_for(role)
+        if channel.permissions_for(role).connect == True:
+            await channel.set_permissions(role, connect=False, view_channel=permissions.view_channel)
+        elif channel.permissions_for(role).connect == False:
+            await channel.set_permissions(role, connect=True, view_channel=permissions.view_channel)
+        if self.current_response and ctx.user.id == self.current_response.user.id: await self.current_response.delete_original_response()
+        self.current_response = await ctx.response.send_message(content="privacy changed", ephemeral=True)
+
+    @permission_check
+    async def visibility(self, ctx: discord.Interaction, channel_owner):
+        channel: discord.VoiceChannel = channel_owner.get("channel")
+        role: discord.Role = discord.utils.get(ctx.guild.roles, id=config.default_role)
+        permissions: discord.Permissions = channel.permissions_for(role)
+        if channel.permissions_for(role).view_channel == True:
+            await channel.set_permissions(role, view_channel=False, connect=permissions.connect)
+        elif channel.permissions_for(role).view_channel == False:
+            await channel.set_permissions(role, view_channel=True, connect=permissions.connect)
+        if self.current_response and ctx.user.id == self.current_response.user.id: await self.current_response.delete_original_response()
+        self.current_response = await ctx.response.send_message(content="visibility changed", ephemeral=True)
+
+    @permission_check
+    async def region(self, ctx: discord.Interaction, channel_owner):
+        options = [
+            discord.SelectOption(label="automatic", value="0"),
+            discord.SelectOption(label="brazil", value="1"),
+            discord.SelectOption(label="hong kong", value="2"),
+            discord.SelectOption(label="india", value="3"),
+            discord.SelectOption(label="japan", value="4"),
+            discord.SelectOption(label="rotterdam", value="5"),
+            discord.SelectOption(label="russia", value="6"),
+            discord.SelectOption(label="singapore", value="7"),
+            discord.SelectOption(label="south africa", value="8"),
+            discord.SelectOption(label="sydney", value="9"),
+            discord.SelectOption(label="us central", value="10"),
+            discord.SelectOption(label="us east", value="11"),
+            discord.SelectOption(label="us south", value="12"),
+            discord.SelectOption(label="us west", value="13")
+            ]
+        select = Select(placeholder="select an option", options=options)
+        async def callback(interaction: discord.Interaction):
+            selected = interaction.data["values"][0]
+            channel: discord.VoiceChannel = channel_owner.get("channel")
+            if selected == "0": await channel.edit(rtc_region=None)
+            if selected == "1": await channel.edit(rtc_region="brazil")
+            if selected == "2": await channel.edit(rtc_region="hongkong")
+            if selected == "3": await channel.edit(rtc_region="india")
+            if selected == "4": await channel.edit(rtc_region="japan")
+            if selected == "5": await channel.edit(rtc_region="rotterdam")
+            if selected == "6": await channel.edit(rtc_region="russia")
+            if selected == "7": await channel.edit(rtc_region="singapore")
+            if selected == "8": await channel.edit(rtc_region="southafrica")
+            if selected == "9": await channel.edit(rtc_region="sydney")
+            if selected == "10": await channel.edit(rtc_region="us-central")
+            if selected == "11": await channel.edit(rtc_region="us-east")
+            if selected == "12": await channel.edit(rtc_region="us-south")
+            if selected == "13": await channel.edit(rtc_region="us-west")
+            if self.current_response and ctx.user.id == self.current_response.user.id: await self.current_response.delete_original_response()
+            self.current_response = await interaction.response.send_message("region changed", ephemeral=True)
+        select.callback = callback
+        view = View()
+        view.add_item(select)
+        if self.current_response and ctx.user.id == self.current_response.user.id: await self.current_response.delete_original_response()
+        self.current_response = await ctx.response.send_message("choose a region:", view=view, ephemeral=True)
+
+    @permission_check
+    async def allow(self, ctx: discord.Interaction, channel_owner):
         pass
 
-    async def private():
+    @permission_check
+    async def forbid(self, ctx: discord.Interaction, channel_owner):
         pass
 
-    async def visibility():
+    @permission_check
+    async def transfer(self, ctx: discord.Interaction, channel_owner):
         pass
 
-    async def region():
+    @permission_check
+    async def kick(self, ctx: discord.Interaction, channel_owner):
         pass
 
-    async def allow():
-        pass
-
-    async def forbid():
-        pass
-
-    async def transfer():
-        pass
-
-    async def kick():
-        pass
-
-    async def delete():
-        pass
+    @permission_check
+    async def delete(self, ctx: discord.Interaction, channel_owner):
+        self.temp_channels.remove(channel_owner)
+        await self.voice_delete(channel_owner.get("channel"))
+        if self.current_response and ctx.user.id == self.current_response.user.id: await self.current_response.delete_original_response()
+        self.current_response = await ctx.response.send_message(content="channel deleted", ephemeral=True)
