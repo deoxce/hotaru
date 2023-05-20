c65786dd991c71d1426f4a6836b9379dfa645d87
Andrew Tolkachew <andrey.tolkachev899@gmail.com>
2023-05-20 22:41:07 +0300

    added persistent view

--- verification.py
+++ verification.py
@@ -15,16 +15,19 @@ class verification(commands.Cog):
 
         emoji_verified = discord.PartialEmoji(name="verified", animated=False, id=1105969558692569198)
         verif_view = View(timeout=None)
-        button_start = Button(label="verify", style=discord.ButtonStyle.blurple, emoji=emoji_verified)
+        button_start = Button(label="verify", custom_id="verification_button", style=discord.ButtonStyle.blurple, emoji=emoji_verified)
         button_start.callback = self.give_role
         verif_view.add_item(button_start)
 
+        # раскомментить если нету сообщения
         await self.client.get_channel(config.verification_channel).purge(limit=100)
         await self.client.get_channel(config.verification_channel).send(view=verif_view, embed=verif_embed)
+
+        #self.client.add_view(verif_view)
         print("ready")
     
     async def give_role(self, ctx: discord.Interaction):
-        role = discord.utils.get(ctx.guild.roles, name=config.default_role)
+        role = discord.utils.get(ctx.guild.roles, id=config.default_role)
         await ctx.user.add_roles(role)
         await ctx.response.send_message("done", ephemeral=True)
         await asyncio.sleep(5)
